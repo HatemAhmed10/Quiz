e142f18e76483b0916ba0e7f6e25f2a3799b25f1
Hatem <hatemahmedmohamed27@gmail.com>
2023-03-15 20:26:09 +0200

    Splash_And_Start_Screen

---
+++ lib/shared/styles/assets_manager.dart
@@ -0,0 +1,9 @@
+const String imagePath = "assets/images/Type_Screen";
+
+// هنا هضيف اي لينك لصوره عندي عشان اتحكم فيهم من هنا بس
+class ImageAssets {
+  // static const String splashLogo = "$imagePath/splash_logo.png";
+  static const String Learn_Image = "$imagePath/learn.jpg";
+  static const String News_Image = "$imagePath/News.jpg";
+  static const String Quiz_Image = "$imagePath/Quiz.jpg";
+}
